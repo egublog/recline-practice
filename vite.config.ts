c25a95fcc6b6cfd42b6a6589212526bd3b0bd09a
egublog <egukei0729@gmail.com>
2025-01-20 00:35:48 +0900

    build: プロジェクト設定ファイルの更新

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,20 @@
-import path from "path"
-import { defineConfig } from "vite"
-import react from "@vitejs/plugin-react"
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': '/src',
     },
   },
+  server: {
+    watch: {
+      usePolling: true,
+    },
+  },
+  optimizeDeps: {
+    include: ['i18next', 'react-i18next'],
+  }
 })
