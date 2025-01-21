58691d2dafdb0c8ae6ff0b51d23ba8be201fc0fa
egublog <egukei0729@gmail.com>
2025-01-21 22:21:28 +0900

    config: 多言語対応の設定を更新
    - i18n設定ファイルを更新
    - アプリケーションのエントリーポイントに多言語サポートを追加
    - 言語切り替え機能の基盤を整備

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,22 @@
-import React from "react"
-import ReactDOM from "react-dom/client"
-import { ChakraProvider } from "@chakra-ui/react"
-import App from "./App"
-import "./lib/i18n/config"
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { ChakraProvider } from "@chakra-ui/react";
+import App from "./App";
+import "./lib/i18n/config";
+import { TodoProvider } from "./contexts/TodoContext";
+import { I18nProvider } from "./contexts/I18nContext";
+import { ThemeProvider } from "./contexts/ThemeContext";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ChakraProvider>
-      <App />
+      <ThemeProvider>
+        <I18nProvider>
+          <TodoProvider>
+            <App />
+          </TodoProvider>
+        </I18nProvider>
+      </ThemeProvider>
     </ChakraProvider>
   </React.StrictMode>
-)
+);
